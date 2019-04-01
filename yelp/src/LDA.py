47afdb4cbfcea76bbab2a3983d3d0993ac2c9c21
Jae Wook Lee <jlee759@illinois.edu>
2019-03-31 23:10:37 -0500

    remove irrelavant file

--- yelp/src/LDA.py
+++
@@ -1,6 +0,0 @@
-# Modified code from https://towardsdatascience.com/topic-modeling-and-latent-dirichlet-allocation-in-python-9bf156893c24
-
-import gensim
-from proprocess import preprocess
-import numpy as np
-from gensim import corpora, models
